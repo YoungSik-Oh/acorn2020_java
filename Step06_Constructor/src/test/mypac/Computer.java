2d047eb0c8caca913c3faacd476cdeaa3ee0e6af
YoungSik <dudtlr9308@naver.com>
2020-06-02 12:10:51 +0900

    20-06-20 오전 java 수업

--- Step06_Constructor/src/test/mypac/Computer.java
+++ Step06_Constructor/src/test/mypac/Computer.java
@@ -3,11 +3,15 @@ package test.mypac;
 public class Computer {
 	// field 
 	public Cpu cpu;
+	// 기본 생성자(인자로 아무것도 전달 받지 않는 생성자)
+	public Computer () {
+		System.out.println("기본 생성자가 호출 되었쓰ㅃ니따 ");
+	}
 	// constructor
 	public Computer (Cpu cpu) {
 		this.cpu=cpu;
 	}
-	//method
+	//method`
 	public void doGame() {
 		if(this.cpu==null) {
 			System.out.println("Cpu가 없어서 컴퓨터가 동작 안함요");
